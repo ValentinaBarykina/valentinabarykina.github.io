f9ffbd0c8dbdcf73cce1ac4f6b4a9a2b25d49f58
khesualdobarykin@gmail.com <medvegalapa@gmail.com>
2016-06-03 21:34:21 -0600

    initial commit

---
+++ Javascript/main.js
@@ -0,0 +1,30 @@
+
+// Toggles the sidebar when on small screens
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+// For dropdown list of recipes
+$("#recipe-toggle").click(function(e){
+	e.preventDefault();
+	$("#recipe-menu").toggle();
+
+	if($("#recipe-menu").is(":visible")){
+		$(this).text("");
+		$(this).text("Recipes");
+		$(this).append("<span class=\"dropup\"><span class=\"caret\"></span></span>");
+	}
+	else{
+		$(this).text("");
+		$(this).text("Recipes");
+		$(this).append("<span class=\"dropdown\"><span class=\"caret\"></span></span>");
+	}
+});
+
+// For the about section
+$('.nav-stacked li').on('click', function(){
+    var target = $(this).children().attr("href");
+    $(target).show().siblings().hide();
+    $(this).addClass("active").siblings().removeClass("active");
+});
